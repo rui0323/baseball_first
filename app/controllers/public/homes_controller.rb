f8a6eb0b5415477c46198ea8ae276e5bf3a51992
rui0323 <rui19990323@icloud.com>
2023-03-27 07:32:42 +0000

    admin

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,2 +1,13 @@
 class Public::HomesController < ApplicationController
+  def top
+    @items = Item.where(is_active: true)
+    @genres = Genre.all
+    if params[:genre_id]
+    @items = @items.where(genre_id: params[:genre_id]).page(params[:page])
+    @title = Genre.find(params[:genre_id]).name
+    else
+    @items = @items.order('id DESC').limit(4)
+    @title = "新着商品"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
    get "/orders/complete" => "orders#complete"
   resources :orders, only: [:new, :create, :show, :index]
    get 'search' => "searches#search"
-   root to: "homes#top"
+   root "homes#top"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
